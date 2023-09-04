667eeebe617dd7295eba4fcd559eff413c033919
Schappo1612 <gustavoschappo1612@gmail.com>
2023-09-04 15:04:54 -0300

    15

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from usuario.router import router as usuario_router
 
 from rest_framework.routers import DefaultRouter
 
@@ -12,4 +13,5 @@ router.register(r"fornecedores", FornecedorViewSet),
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
+    path("api/", include(usuario_router.urls)),
 ]
