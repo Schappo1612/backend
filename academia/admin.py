fb1621c5f998e1fa40c61e90e553c407d5717be7
Schappo1612 <gustavoschappo1612@gmail.com>
2023-09-04 13:15:48 -0300

    6.5

--- academia/admin.py
+++ academia/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from academia.models import Categoria, Fornecedor
+from academia.models import Categoria, Fornecedor, Produtos
 
 admin.site.register(Categoria)
 admin.site.register(Fornecedor)
+admin.site.register(Produtos)
