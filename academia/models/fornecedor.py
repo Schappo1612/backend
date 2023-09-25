5b4c15b6005b10c2278bf6c75547dd65d9647b8f
Schappo1612 <gustavoschappo1612@gmail.com>
2023-09-25 09:14:25 -0300

    ttt

--- academia/models/fornecedor.py
+++ academia/models/fornecedor.py
@@ -2,7 +2,9 @@ from django.db import models
 
 class Fornecedor(models.Model):
     nome = models.CharField(max_length=255)
-    email = models.EmailField()
+    # email = models.EmailField()
+    claudio = models.ImageField(upload_to=(''), null=True, blank=True)
+
 
     def __str__(self):
         return self.nome
