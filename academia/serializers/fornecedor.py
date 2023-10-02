94615b76eea8b68f61a3a753cc83bd4a307b9fb6
Schappo1612 <gustavoschappo1612@gmail.com>
2023-10-02 09:14:04 -0300

    fornecedor

--- academia/serializers/fornecedor.py
+++ academia/serializers/fornecedor.py
@@ -1,8 +1,10 @@
-from rest_framework.serializers import ModelSerializer
-
+from rest_framework.serializers import ModelSerializer, CharField
+from uploader.serializers import ImageSerializer
 from academia.models import Categoria, Fornecedor, Produtos
 
 class FornecedorSerializer(ModelSerializer):
+    # imagem = CharField(source="imagem.file", read_only = True)
     class Meta:
         model = Fornecedor
-        fields = "__all__"
+        fields = ["id", "nome", "imagem"]
+        depth = 1
