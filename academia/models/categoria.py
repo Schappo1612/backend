9fc76bff8742278a41557eb8deed67d6e5680d24
Schappo1612 <gustavoschappo1612@gmail.com>
2023-10-02 14:38:22 -0300

    categorias

--- academia/models/categoria.py
+++ academia/models/categoria.py
@@ -4,6 +4,7 @@ from uploader.models import Image
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
+    imagem = models.ForeignKey(Image, on_delete=models.PROTECT, null=True, blank=True, default=None)
 
     def __str__(self):
         return self.descricao
--- academia/serializers/categoria.py
+++ academia/serializers/categoria.py
@@ -1,8 +1,10 @@
 from rest_framework.serializers import ModelSerializer
 
 from academia.models import Categoria, Fornecedor, Produtos
+from uploader.serializers import ImageSerializer
 
 class CategoriaSerializer(ModelSerializer):
+    imagem = ImageSerializer()
     class Meta:
         model = Categoria
         fields = "__all__"
